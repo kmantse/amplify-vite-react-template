56a7dc86d820b658bd686be4f5f170d8c21f70d7
kmantse <knimoh123@gmail.com>
2026-05-02 12:19:18 +0000

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -26,11 +26,11 @@ function App() {
   }
 
   //signout variable
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
